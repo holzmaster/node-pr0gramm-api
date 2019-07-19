6a90d2eb4a9920d9483dd66c61024f4a806968f5
holzmaster <holzmaster@outlook.com>
2019-07-20 01:01:17 +0200

    Fix path

--- src/client-constants.ts
+++ src/client-constants.ts
@@ -1,4 +1,4 @@
-const VERSION = require("../../package.json").version;
+const VERSION = require("../package.json").version;
 
 export const PR0GRAMM_BASE_URL = "https://pr0gramm.com";
 export const PR0GRAMM_API_PATH = "/api";
