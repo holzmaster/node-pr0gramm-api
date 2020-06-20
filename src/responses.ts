d4f473b4abf94492af812e92469e976076ea2f11
holzmaster <holzmaster@outlook.com>
2020-06-20 19:37:43 +0200

    Update responses.ts

--- src/responses.ts
+++ src/responses.ts
@@ -3,8 +3,10 @@ import * as Types from "./common-types";
 export interface Pr0grammResponse {
 	ts: Types.Timestamp;
 	cache: Types.Cache;
-	rt: Types.Roundtrip;
-	qc: number; // ??
+	/** Response Time */
+	rt: number;
+	/** Query count */
+	qc: number;
 }
 
 export interface GetItemsInfoResponse extends Pr0grammResponse {
