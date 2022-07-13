7692fbb667dc35a02a948068c5cd4b9def3cb311
holzmaster <holzmaster@outlook.com>
2022-07-13 03:41:01 +0200

    Inline some vars

--- src/pr0gramm-api.ts
+++ src/pr0gramm-api.ts
@@ -1,4 +1,3 @@
-
 import * as Response from "./responses";
 import * as Types from "./common-types";
 import { APIRequester } from "./requester";
@@ -10,7 +9,6 @@ import { ensureUnixTimetamp, createTagList } from "./util";
  * If you don't need the whole API, you can instantiate the services yourself (e. g. Pr0grammItemsService).
  */
 export class Pr0grammAPI {
-
 	public readonly items: Pr0grammItemsService;
 	public readonly tags: Pr0grammTagsService;
 	public readonly messages: Pr0grammMessageService;
@@ -38,47 +36,36 @@ export class Pr0grammAPI {
 
 
 export class Pr0grammItemsService {
-	constructor(private readonly requester: APIRequester) {
-	}
+	constructor(private readonly requester: APIRequester) { }
 
 	public delete(options: DeleteItemOptions): Promise<Response.Pr0grammResponse> {
-		const path = `/items/delete`;
-		return this.requester.post(path, options);
+		return this.requester.post("/items/delete", options);
 	}
 
 	public getInfo(itemId: Types.ItemID): Promise<Response.GetItemsInfoResponse> {
-		const path = `/items/info`;
-		return this.requester.get(path, { itemId });
+		return this.requester.get("/items/info", { itemId });
 	}
 
 	public getItems(options: GetItemsOptions): Promise<Response.GetItemsResponse> {
-		const path = `/items/get`;
-		const rawOptions = Pr0grammItemsService.parseRawGetItemsOptions(options);
-		return this.requester.get(path, rawOptions);
+		return this.requester.get("/items/get", Pr0grammItemsService.parseRawGetItemsOptions(options));
 	}
 	public getItemsNewer(options: GetItemsNewerOptions): Promise<Response.GetItemsResponse> {
-		const path = `/items/get`;
-		const rawOptions = {
+		return this.requester.get("/items/get", {
 			newer: options.newer,
-			...Pr0grammItemsService.parseRawGetItemsOptions(options)
-		};
-		return this.requester.get(path, rawOptions);
+			...Pr0grammItemsService.parseRawGetItemsOptions(options),
+		});
 	}
 	public getItemsOlder(options: GetItemsOlderOptions): Promise<Response.GetItemsResponse> {
-		const path = `/items/get`;
-		const rawOptions = {
+		return this.requester.get("/items/get", {
 			older: options.older,
-			...Pr0grammItemsService.parseRawGetItemsOptions(options)
-		};
-		return this.requester.get(path, rawOptions);
+			...Pr0grammItemsService.parseRawGetItemsOptions(options),
+		});
 	}
 	public getItemsAround(options: GetItemsAroundOptions): Promise<Response.GetItemsResponse> {
-		const path = `/items/get`;
-		const rawOptions = {
+		return this.requester.get("/items/get", {
 			id: options.around,
-			...Pr0grammItemsService.parseRawGetItemsOptions(options)
-		};
-		return this.requester.get(path, rawOptions);
+			...Pr0grammItemsService.parseRawGetItemsOptions(options),
+		});
 	}
 
 	public walkStreamNewer(options: GetItemsNewerOptions): AsyncIterableIterator<Types.Item> {
@@ -95,10 +82,10 @@ export class Pr0grammItemsService {
 		return this.walkStream(
 			options.older,
 			options, {
-				getNextId: res => res.items[res.items.length - 1].id,
-				shouldContinue: res => !res.atEnd,
-				getItems: (opts, currentId) => this.getItemsOlder({ ...opts, older: currentId }),
-			}
+			getNextId: res => res.items[res.items.length - 1].id,
+			shouldContinue: res => !res.atEnd,
+			getItems: (opts, currentId) => this.getItemsOlder({ ...opts, older: currentId }),
+		}
 		);
 	}
 
@@ -141,16 +128,13 @@ export class Pr0grammItemsService {
 	}
 
 	public vote(id: Types.ItemID, absoluteVote: Types.Vote): Promise<Response.Pr0grammResponse> {
-		const path = `/items/vote`;
-		const data = {
+		return this.requester.post("/items/vote", {
 			id,
 			vote: absoluteVote,
-		};
-		return this.requester.post(path, data);
+		});
 	}
 	public rateLimited(): Promise<Response.Pr0grammResponse> {
-		const path = `/items/ratelimited`;
-		return this.requester.post(path);
+		return this.requester.post("/items/ratelimited");
 	}
 
 	// TODO:
@@ -197,20 +181,17 @@ export interface WalkStreamFunctions {
 }
 
 export class Pr0grammProfileService {
-	constructor(private readonly requester: APIRequester) {
-	}
+	constructor(private readonly requester: APIRequester) { }
 
 	public getCommentsBefore(name: Types.Username, flags: Types.ItemFlags, before: Types.Timestamp): Promise<Response.GetCommentsResponse> {
-		const path = `/profile/comments`;
-		return this.requester.get(path, {
+		return this.requester.get("/profile/comments", {
 			name,
 			flags,
 			before: ensureUnixTimetamp(before),
 		});
 	}
 	public getCommentsAfter(name: Types.Username, flags: Types.ItemFlags, after: Types.Timestamp): Promise<Response.GetCommentsResponse> {
-		const path = `/profile/comments`;
-		return this.requester.get(path, {
+		return this.requester.get("/profile/comments", {
 			name,
 			flags,
 			after: ensureUnixTimetamp(after),
@@ -218,45 +199,36 @@ export class Pr0grammProfileService {
 	}
 
 	public follow(name: Types.Username): Promise<Response.Pr0grammResponse> {
-		const path = `/profile/follow`;
-		return this.requester.post(path, { name });
+		return this.requester.post("/profile/follow", { name });
 	}
 	public unfollow(name: Types.Username): Promise<Response.Pr0grammResponse> {
-		const path = `/profile/unfollow`;
-		return this.requester.post(path, { name });
+		return this.requester.post("/profile/unfollow", { name });
 	}
 
 	public getInfo(name: Types.Username, flags: Types.ItemFlags): Promise<Response.GetProfileInfoResponse> {
-		const path = `/profile/info`;
-		return this.requester.get(path, { name, flags });
+		return this.requester.get("/profile/info", { name, flags });
 	}
 }
 
 export class Pr0grammCommentsService {
-	constructor(private readonly requester: APIRequester) {
-	}
+	constructor(private readonly requester: APIRequester) { }
 
 	public delete(id: Types.CommentID, reason: string): Promise<Response.Pr0grammResponse> {
-		const path = `/comments/delete`;
-		return this.requester.post(path, { id, reason });
+		return this.requester.post("/comments/delete", { id, reason });
 	}
 	public softDelete(id: Types.CommentID, reason: string): Promise<Response.Pr0grammResponse> {
-		const path = `/comments/softDelete`;
-		return this.requester.post(path, { id, reason });
+		return this.requester.post("/comments/softDelete", { id, reason });
 	}
 	public edit(id: Types.CommentID, newContent: string): Promise<Response.Pr0grammResponse> {
-		const path = `/comments/edit`;
-		return this.requester.post(path, { commentId: id, comment: newContent });
+		return this.requester.post("/comments/edit", { commentId: id, comment: newContent });
 	}
 
 	public vote(id: Types.CommentID, absoluteVote: Types.Vote): Promise<Response.Pr0grammResponse> {
-		const path = `/comments/vote`;
-		return this.requester.post(path, { id, vote: absoluteVote, });
+		return this.requester.post("/comments/vote", { id, vote: absoluteVote, });
 	}
 
 	public post(itemId: Types.ItemID, content: string, replyTo: Types.CommentID = 0): Promise<Response.Pr0grammResponse> {
-		const path = `/comments/post`;
-		return this.requester.post(path, {
+		return this.requester.post("/comments/post", {
 			comment: content,
 			itemId,
 			parentId: replyTo,
@@ -270,8 +242,7 @@ export class Pr0grammTagsService {
 	) { }
 
 	public add(itemId: Types.ItemID, tags: readonly Types.TagContent[]): Promise<Response.Pr0grammResponse> {
-		const path = `/tags/add`;
-		return this.requester.post(path, {
+		return this.requester.post("/tags/add", {
 			itemId,
 			tags: createTagList(tags),
 			submit: "Tags speichern",
@@ -280,28 +251,23 @@ export class Pr0grammTagsService {
 
 	/** TODO: This may not work */
 	public delete(itemId: Types.ItemID, banUsers: boolean, days: Types.BanDuration, tags: readonly Types.TagContent[]): Promise<Response.Pr0grammResponse> {
-		const path = `/tags/delete`;
-		return this.requester.post(path, { itemId, tags, banUsers, days });
+		return this.requester.post("/tags/delete", { itemId, tags, banUsers, days });
 	}
 
 	public getDetails(itemId: Types.ItemID): Promise<Response.GetDetailsResponse> {
-		const path = `/tags/details`;
-		return this.requester.get(path, { itemId });
+		return this.requester.get("/tags/details", { itemId });
 	}
 
 	public vote(id: Types.TagID, absoluteVote: Types.Vote): Promise<Response.Pr0grammResponse> {
-		const path = `/tags/vote`;
-		return this.requester.post(path, { id, vote: absoluteVote });
+		return this.requester.post("/tags/vote", { id, vote: absoluteVote });
 	}
 }
 
 export class Pr0grammContactService {
-	constructor(private readonly requester: APIRequester) {
-	}
+	constructor(private readonly requester: APIRequester) { }
 
 	public send(email: Types.Email, subject: string, message: string): Promise<Response.Pr0grammResponse> {
-		const path = `/contact/send`;
-		return this.requester.post(path, { email, subject, message });
+		return this.requester.post("/contact/send", { email, subject, message });
 	}
 }
 
@@ -310,33 +276,27 @@ export class Pr0grammMessageService {
 	}
 
 	public getComments(): Promise<Response.InboxCommentsResponse> {
-		const path = `/inbox/comments`;
-		return this.requester.get(path);
+		return this.requester.get("/inbox/comments");
 	}
 
 	public getCommentsOlder(older: Types.Timestamp): Promise<Response.InboxCommentsResponse> {
-		const path = `/inbox/comments`;
-		return this.requester.get(path, { older });
+		return this.requester.get("/inbox/comments", { older });
 	}
 
 	public getConversations(): Promise<Response.ConversationResponse> {
-		const path = `/inbox/conversations`;
-		return this.requester.get(path);
+		return this.requester.get("/inbox/conversations");
 	}
 
 	public getConversationsOlder(older: Types.ConversationID): Promise<Response.ConversationResponse> {
-		const path = `/inbox/conversations`;
-		return this.requester.get(path, { older });
+		return this.requester.get("/inbox/conversations", { older });
 	}
 
 	public getMessages(user: Types.Username): Promise<Response.MessagesResponse> {
-		const path = `/inbox/messages`;
-		return this.requester.get(path, { with: user });
+		return this.requester.get("/inbox/messages", { with: user });
 	}
 
 	public sendMessage(recipientName: Types.Username, comment: Types.MessageComment): Promise<Response.MessagesResponse> {
-		const path = `/inbox/post`;
-		return this.requester.post(path, { recipientName, comment });
+		return this.requester.post("/inbox/post", { recipientName, comment });
 	}
 }
 
@@ -345,68 +305,56 @@ export class Pr0grammUserService {
 	}
 
 	public ban(user: Types.Username, reason: string, days: Types.BanDuration): Promise<Response.Pr0grammResponse> {
-		const path = `/user/ban`;
-		return this.requester.post(path, { user, reason, days });
+		return this.requester.post("/user/ban", { user, reason, days });
 	}
 	public changeEmail(token: Types.ChangeEmailToken): Promise<Response.ChangeUserDataResponse> {
-		const path = `/user/changeemail`;
-		return this.requester.post(path, { token });
+		return this.requester.post("/user/changeemail", { token });
 	}
 	public changePassword(newPassword: Types.Password): Promise<Response.ChangeUserDataResponse> {
-		const path = `/user/changepassword`;
-		return this.requester.post(path, { password: newPassword });
+		return this.requester.post("/user/changepassword", { password: newPassword });
 	}
 
 	public getFollowList(flags: Types.ItemFlags): Promise<Response.GetFollowListReponse> {
-		const path = `/user/followlist`;
-		return this.requester.get(path, { flags });
+		return this.requester.get("/user/followlist", { flags });
 	}
 
 	public getInfo(): Promise<Response.GetUserInfoResponse> {
-		const path = `/user/info`;
-		return this.requester.get(path);
+		return this.requester.get("/user/info");
 	}
 
 	public invite(email: Types.Email): Promise<Response.ChangeUserDataResponse> {
-		const path = `/user/invite`;
-		return this.requester.post(path, { email });
+		return this.requester.post("/user/invite", { email });
 	}
 
 	/**
 	 * ????
 	 */
 	public joinWithInvite(token: Types.InviteToken, email: Types.Email, password: Types.Password, name: Types.Username): Promise<Response.ChangeUserDataResponse> {
-		const path = `/user/joinwithinvite`;
-		return this.requester.post(path, { token, email, password, name });
+		return this.requester.post("/user/joinwithinvite", { token, email, password, name });
 	}
 	/**
 	 * ????
 	 */
 	public joinWithToken(token: Types.InviteToken, email: Types.Email, password: Types.Password, name: Types.Username): Promise<Response.TokenResponse> {
-		const path = `/user/joinwithtoken`;
-		return this.requester.post(path, { token, email, password, name });
+		return this.requester.post("/user/joinwithtoken", { token, email, password, name });
 	}
 
 	public loadInvite(token: Types.InviteToken): Promise<Response.LoadInviteResponse> {
-		const path = `/user/loadinvite`;
-		return this.requester.get(path, { token });
+		return this.requester.get("/user/loadinvite", { token });
 	}
 
 	public loadPaymentToken(token: Types.PaymentToken): Promise<Response.TokenInfoResponse> {
-		const path = `/user/loadpaymenttoken`;
-		return this.requester.post(path, { token });
+		return this.requester.post("/user/loadpaymenttoken", { token });
 	}
 
 	public requestCaptcha(): Promise<CaptchaResponse> {
-		const path = `/user/captcha`;
-		return this.requester.post(path, undefined, true);
+		return this.requester.post("/user/captcha", undefined, true);
 	}
 
 	public login(name: Types.Username, password: Types.Password): Promise<Response.LogInResponse>;
 	public login(name: Types.Username, password: Types.Password, captchaToken: CaptchaResponse["token"], captchaSolution: string): Promise<Response.LogInResponse>;
 	public login(name: Types.Username, password: Types.Password, captchaToken?: CaptchaResponse["token"], captchaSolution?: string): Promise<Response.LogInResponse> {
-		const path = `/user/login`;
-		return this.requester.post(path, {
+		return this.requester.post("/user/login", {
 			name,
 			password,
 			token: captchaToken,
@@ -415,51 +363,43 @@ export class Pr0grammUserService {
 	}
 
 	public logout(id: Types.SessionID): Promise<Response.Pr0grammResponse> {
-		const path = `/user/logout`;
-		return this.requester.post(path, { id });
+		return this.requester.post("/user/logout", { id });
 	}
 
 	/**
 	 * ????
 	 */
 	public redeemToken(token: Types.InviteToken): Promise<Response.TokenResponse> {
-		const path = `/user/redeemtoken`;
-		return this.requester.post(path, { token });
+		return this.requester.post("/user/redeemtoken", { token });
 	}
 
 	public requestEmailChange(newEmail: Types.Email): Promise<Response.ChangeUserDataResponse> {
-		const path = `/user/requestemailchange`;
-		return this.requester.post(path, { email: newEmail });
+		return this.requester.post("/user/requestemailchange", { email: newEmail });
 	}
 
 	public resetPassword(name: Types.Username, password: Types.Password, token: Types.ChangePasswordToken): Promise<Response.ChangeUserDataResponse> {
-		const path = `/user/resetpassword`;
-		return this.requester.post(path, { name, password, token });
+		return this.requester.post("/user/resetpassword", { name, password, token });
 	}
 
 	public sendPasswordResetMail(email: Types.Email): Promise<Response.Pr0grammResponse> {
-		const path = `/user/sendpasswordresetmail`;
-		return this.requester.post(path, { email }, true);
+		return this.requester.post("/user/sendpasswordresetmail", { email }, true);
 	}
 
 	public setSiteSettings(siteSettings: SiteSettingsOptions): Promise<Response.ChangeUserDataResponse> {
-		const path = `/user/sitesettings`;
 		const options = {
 			likesArePublic: siteSettings.likesArePublic,
 			showAds: siteSettings.showAds,
 			userStatus: "um" + siteSettings.userStatus
 		};
-		return this.requester.post(path, options);
+		return this.requester.post("/user/sitesettings", options);
 	}
 
 	public sync(offset: Types.SyncID): Promise<Response.SyncResponse> {
-		const path = `/user/sync`;
-		return this.requester.get(path, { offset });
+		return this.requester.get("/user/sync", { offset });
 	}
 
 	public validate(token: Types.Token): Promise<Response.SuccessableResponse> {
-		const path = `/user/validate`;
-		return this.requester.post(path, { token });
+		return this.requester.post("/user/validate", { token });
 	}
 
 	public getIdentifier(): Promise<Response.GetIdentifierResponse> {
